65c6e616e9957ad72377cbd9d7ba851b51679573
Cam <cginiel@umich.edu>
2020-04-25 18:55:14 -0400

    caching, begin database

--- finalproject.py
+++ finalproject.py
@@ -1,16 +1,24 @@
+#############################################################
+################    Name: Cameron Giniel       ##############
+################    Uniqname: cginiel          ##############
+#############################################################
+
 from bs4 import BeautifulSoup
 import requests
 import sys
 import secrets
-import csv
+import json
+import sqlite3
 
 ################## global vars ##############################
-CACHE_FILENAME = "cache.json"
+CACHE_FILENAME = "covid_cache.json"
 CACHE_DICT = {}
 
 API_KEY = secrets.rapid_api_key
 covid_url = "https://covid-193.p.rapidapi.com/statistics"
-#############################################################
+
+DB_NAME = 'covid_stats.sqlite'
+########### data gathering and sorting #################
 
 def scrape_wiki_data():
     '''Parses wikipedia page for table contents.
@@ -68,7 +76,6 @@ def scrape_wiki_data():
     # to make dict: think about how you can create a dictionary using the cleaned_headings for keys and the rows of the txt doc for values.
 
 
-
 def make_request(base_url):
     '''Calls the rapid api and returns a json 
     of coronavirus cases by country and by continent and even a cruise ship.
@@ -90,35 +97,206 @@ def make_request(base_url):
     }
 
     response = requests.request("GET", base_url, headers=headers)
+    make_request_with_cache(base_url, response.json())
+
     return response.json()
 
-if __name__ == '__main__':
-    ## light cleaning of the API json
 
-    all_countries = make_request(covid_url)['response']
+def create_covid_cases_dict(covid_json):
+    '''Pulls a json from the covid API and returns a cleaned dictonary.
+
+    params
+    ------
+    None
+
+    returns
+    -------
+    dict
+        a cleaned dictionary with a country as the key and 
+        the values as corresponding new cases, new deaths,
+        active cases, and total cases
+    '''
+    # all_countries = make_request(covid_url)['response']
+    all_countries = covid_json['response']
+    make_request_with_cache(covid_url, all_countries)
+
     countries_list = []
     new_cases_list = []
     active_cases_list = []
     total_cases_list = []
     new_deaths_list = []
+
+    # add our data to their own lists
     for country in all_countries:
         countries_list.append(country['country'])
-        new_cases_list.append(country['cases']['new']) # need to remove plus signs
         active_cases_list.append(country['cases']['active'])
         total_cases_list.append(country['cases']['total'])
-        new_deaths_list.append(country['deaths']['new']) # need to remove plus signs
-    for n in new_cases_list:
-        if n != None:
-            if "+" in n:
-                n = n[1:]
-                new_cases_list.append(n)
-    print(new_cases_list)
-        # new_cases_list.append(n)
 
-    # print(new_deaths_list)
-    # print(all_countries)
+        # clean the plus sign off of new cases
+        new_cases = country['cases']['new']
+        if new_cases != None:
+            new_cases = new_cases[1:]
+        new_cases_list.append(new_cases)
+
+        # clean the plus sign off of new deaths
+        new_deaths = country['deaths']['new']
+        if new_deaths != None:
+            new_deaths = new_deaths[1:]
+        new_deaths_list.append(new_deaths)
+
     covid_dict = {}
-    # scrape_wiki_data()
+
+    for i in range(len(countries_list)):
+        covid_dict[countries_list[i]] = {
+            'new cases': new_cases_list[i],
+            'active cases': active_cases_list[i],
+            'new deaths': new_deaths_list[i],
+            'total cases': total_cases_list[i]
+        }
+
+    return covid_dict
+
+
+###################### caching ##########################
+def open_cache():
+    ''' Opens the cache file if it exists and loads the JSON into
+    the CACHE_DICT dictionary.
+    if the cache file doesn't exist, creates a new cache dictionary
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    dict
+        the opened cache
+    '''
+    try:
+        cache_file = open(CACHE_FILENAME, 'r')
+        cache_contents = cache_file.read()
+        cache_dict = json.loads(cache_contents)
+        cache_file.close()
+    except:
+        cache_dict = {}
+    return cache_dict
+
+
+def save_cache(cache_dict):
+    ''' Saves the current state of the cache to disk
+    
+    Parameters
+    ----------
+    cache_dict: dict
+        The dictionary to save
+    
+    Returns
+    -------
+    None
+    '''
+    dumped_json_cache = json.dumps(cache_dict)
+    fw = open(CACHE_FILENAME,"w")
+    fw.write(dumped_json_cache)
+    fw.close() 
+
+
+def make_request_with_cache(key, value):
+    '''Issues a request to the cache saved to the device.
+
+    If the item exists in the cache, the program will pull from that data.
+    If the item does not exist in the cache, the program will create a key/value
+    pair and save it to the cache dictionary
+
+    Parameters
+    ----------
+    key
+        list
+    value
+        list
+
+    '''
+
+    if key in CACHE_DICT.keys():
+        print("Using cache")
+        return CACHE_DICT[key]
+    else:
+        print("Fetching")
+        CACHE_DICT[key] = value
+        save_cache(CACHE_DICT)
+        return CACHE_DICT[key]
+
+
+##################### database ##########################
+def create_db():
+    ''' Create a SQL database if it doesn't already exist and populate data in the tables.
+    If the database already exists, the function writes over the existing data.
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    None
+    '''
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+
+    drop_covid_cases_sql = 'DROP TABLE IF EXISTS "Cases"'
+    
+
+    create_covid_cases_sql = '''
+        CREATE TABLE IF NOT EXISTS "Cases" (
+            "Id" INTEGER PRIMARY KEY AUTOINCREMENT,
+            "Country" TEXT NOT NULL,
+            "NewCases" INTEGER NULL,
+            "ActiveCases" INTEGER NULL,
+            "NewDeaths" INTEGER NULL,
+            "TotalCases" INTEGER NOT NULL
+        )
+    '''
+
+    cur.execute(drop_covid_cases_sql)
+    cur.execute(create_covid_cases_sql)
+    conn.commit()
+    conn.close()
+
+def load_cases():
+    covid_dict = create_covid_cases_dict(make_request(covid_url))
+
+    insert_sql = '''
+        INSERT INTO Cases
+        VALUES (NULL, ?, ?, ?, ?, ?)
+    '''
+
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+
+    for k,v in covid_dict.items():
+        cur.execute(insert_sql,[
+            k,
+            v['new cases'],
+            v['active cases'],
+            v['new deaths'],
+            v['total cases']
+            ]
+        )
+
+
+if __name__ == '__main__':
+    conn = sqlite3.connect(DB_NAME)
+    CACHE_DICT = open_cache()
+    covid_dict = create_covid_cases_dict(make_request(covid_url))
+    create_db()
+    load_cases()
+    conn.commit()
+    conn.close()
+    # print(covid_dict)
+    
+
+
+    
+
     
 
 
